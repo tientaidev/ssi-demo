f0ebf99422eb309c81e37b30723526def6e74c21
tientaidev <tientaidev@gmail.com>
2022-05-23 23:29:02 +0700

    Edit link in sidebar

--- Issuer/frontend/src/components/dashboard/dashboard-sidebar.tsx
+++ Issuer/frontend/src/components/dashboard/dashboard-sidebar.tsx
@@ -5,34 +5,15 @@ import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import type { TFunction } from 'react-i18next';
-import { Box, Button, Chip, Divider, Drawer, Typography, useMediaQuery } from '@mui/material';
+import { Box, Button, Divider, Drawer, Typography, useMediaQuery } from '@mui/material';
 import type { Theme } from '@mui/material';
-import { Calendar as CalendarIcon } from '../../icons/calendar';
-import { Cash as CashIcon } from '../../icons/cash';
-import { ChartBar as ChartBarIcon } from '../../icons/chart-bar';
-import { ChartPie as ChartPieIcon } from '../../icons/chart-pie';
-import { ChatAlt2 as ChatAlt2Icon } from '../../icons/chat-alt2';
-import { ClipboardList as ClipboardListIcon } from '../../icons/clipboard-list';
-import { CreditCard as CreditCardIcon } from '../../icons/credit-card';
-import { Home as HomeIcon } from '../../icons/home';
-import { LockClosed as LockClosedIcon } from '../../icons/lock-closed';
-import { Mail as MailIcon } from '../../icons/mail';
-import { MailOpen as MailOpenIcon } from '../../icons/mail-open';
-import { Newspaper as NewspaperIcon } from '../../icons/newspaper';
-import { OfficeBuilding as OfficeBuildingIcon } from '../../icons/office-building';
-import { ReceiptTax as ReceiptTaxIcon } from '../../icons/receipt-tax';
 import { Selector as SelectorIcon } from '../../icons/selector';
-import { Share as ShareIcon } from '../../icons/share';
 import { ShoppingBag as ShoppingBagIcon } from '../../icons/shopping-bag';
-import { ShoppingCart as ShoppingCartIcon } from '../../icons/shopping-cart';
-import { Truck as TruckIcon } from '../../icons/truck';
-import { UserCircle as UserCircleIcon } from '../../icons/user-circle';
-import { Users as UsersIcon } from '../../icons/users';
-import { XCircle as XCircleIcon } from '../../icons/x-circle';
 import { Logo } from '../logo';
 import { Scrollbar } from '../scrollbar';
 import { DashboardSidebarSection } from './dashboard-sidebar-section';
 import { OrganizationPopover } from './organization-popover';
+import { NumbersOutlined, Key, VerifiedUser } from '@mui/icons-material'
 
 interface DashboardSidebarProps {
   onClose?: () => void;
@@ -54,255 +35,25 @@ interface Section {
 
 const getSections = (t: TFunction): Section[] => [
   {
-    title: t('General'),
-    items: [
-      {
-        title: t('Overview'),
-        path: '/dashboard',
-        icon: <HomeIcon fontSize="small" />
-      },
-      {
-        title: t('Analytics'),
-        path: '/dashboard/analytics',
-        icon: <ChartBarIcon fontSize="small" />
-      },
-      {
-        title: t('Finance'),
-        path: '/dashboard/finance',
-        icon: <ChartPieIcon fontSize="small" />
-      },
-      {
-        title: t('Logistics'),
-        path: '/dashboard/logistics',
-        icon: <TruckIcon fontSize="small" />,
-        chip: <Chip
-          color="secondary"
-          label={(
-            <Typography
-              sx={{
-                fontSize: '10px',
-                fontWeight: '600'
-              }}
-            >
-              NEW
-            </Typography>
-          )}
-          size="small"
-        />
-      },
-      {
-        title: t('Account'),
-        path: '/dashboard/account',
-        icon: <UserCircleIcon fontSize="small" />
-      }
-    ]
-  },
-  {
     title: t('Management'),
     items: [
       {
-        title: t('Customers'),
+        title: 'Decentralized Identifiers',
         path: '/dashboard/customers',
-        icon: <UsersIcon fontSize="small" />,
-        children: [
-          {
-            title: t('List'),
-            path: '/dashboard/customers'
-          },
-          {
-            title: t('Details'),
-            path: '/dashboard/customers/1'
-          },
-          {
-            title: t('Edit'),
-            path: '/dashboard/customers/1/edit'
-          }
-        ]
+        icon: <NumbersOutlined fontSize="small" />,
       },
       {
-        title: t('Products'),
+        title: 'Verifiable Credentials',
         path: '/dashboard/products',
-        icon: <ShoppingBagIcon fontSize="small" />,
-        children: [
-          {
-            title: t('List'),
-            path: '/dashboard/products'
-          },
-          {
-            title: t('Create'),
-            path: '/dashboard/products/new'
-          }
-        ]
+        icon: <VerifiedUser fontSize="small" />,
       },
       {
-        title: t('Orders'),
-        icon: <ShoppingCartIcon fontSize="small" />,
+        title: 'Keys',
+        icon: <Key fontSize="small" />,
         path: '/dashboard/orders',
-        children: [
-          {
-            title: t('List'),
-            path: '/dashboard/orders'
-          },
-          {
-            title: t('Details'),
-            path: '/dashboard/orders/1'
-          }
-        ]
-      },
-      {
-        title: t('Invoices'),
-        path: '/dashboard/invoices',
-        icon: <ReceiptTaxIcon fontSize="small" />,
-        children: [
-          {
-            title: t('List'),
-            path: '/dashboard/invoices'
-          },
-          {
-            title: t('Details'),
-            path: '/dashboard/invoices/1'
-          }
-        ]
       }
     ]
   },
-  {
-    title: t('Platforms'),
-    items: [
-      {
-        title: t('Job Listings'),
-        path: '/dashboard/jobs',
-        icon: <OfficeBuildingIcon fontSize="small" />,
-        children: [
-          {
-            title: t('Browse'),
-            path: '/dashboard/jobs'
-          },
-          {
-            title: t('Details'),
-            path: '/dashboard/jobs/companies/1'
-          },
-          {
-            title: t('Create'),
-            path: '/dashboard/jobs/new'
-          }
-        ]
-      },
-      {
-        title: t('Social Media'),
-        path: '/dashboard/social',
-        icon: <ShareIcon fontSize="small" />,
-        children: [
-          {
-            title: t('Profile'),
-            path: '/dashboard/social/profile'
-          },
-          {
-            title: t('Feed'),
-            path: '/dashboard/social/feed'
-          }
-        ]
-      },
-      {
-        title: t('Blog'),
-        path: '/blog',
-        icon: <NewspaperIcon fontSize="small" />,
-        children: [
-          {
-            title: t('Post List'),
-            path: '/blog'
-          },
-          {
-            title: t('Post Details'),
-            path: '/blog/1'
-          },
-          {
-            title: t('Post Create'),
-            path: '/blog/new'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    title: t('Apps'),
-    items: [
-      {
-        title: t('Kanban'),
-        path: '/dashboard/kanban',
-        icon: <ClipboardListIcon fontSize="small" />
-      },
-      {
-        title: t('Mail'),
-        path: '/dashboard/mail',
-        icon: <MailIcon fontSize="small" />
-      },
-      {
-        title: t('Chat'),
-        path: '/dashboard/chat',
-        icon: <ChatAlt2Icon fontSize="small" />
-      },
-      {
-        title: t('Calendar'),
-        path: '/dashboard/calendar',
-        icon: <CalendarIcon fontSize="small" />
-      }
-    ]
-  },
-  {
-    title: t('Pages'),
-    items: [
-      {
-        title: t('Auth'),
-        path: '/authentication',
-        icon: <LockClosedIcon fontSize="small" />,
-        children: [
-          {
-            title: t('Register'),
-            path: '/authentication/register?disableGuard=true'
-          },
-          {
-            title: t('Login'),
-            path: '/authentication/login?disableGuard=true'
-          }
-        ]
-      },
-      {
-        title: t('Pricing'),
-        path: '/dashboard/pricing',
-        icon: <CreditCardIcon fontSize="small" />
-      },
-      {
-        title: t('Checkout'),
-        path: '/checkout',
-        icon: <CashIcon fontSize="small" />
-      },
-      {
-        title: t('Contact'),
-        path: '/contact',
-        icon: <MailOpenIcon fontSize="small" />
-      },
-      {
-        title: t('Error'),
-        path: '/error',
-        icon: <XCircleIcon fontSize="small" />,
-        children: [
-          {
-            title: '401',
-            path: '/401'
-          },
-          {
-            title: '404',
-            path: '/404'
-          },
-          {
-            title: '500',
-            path: '/500'
-          }
-        ]
-      }
-    ]
-  }
 ];
 
 export const DashboardSidebar: FC<DashboardSidebarProps> = (props) => {
@@ -360,69 +111,6 @@ export const DashboardSidebar: FC<DashboardSidebarProps> = (props) => {
             height: '100%'
           }}
         >
-          <div>
-            <Box sx={{ p: 3 }}>
-              <NextLink
-                href="/"
-                passHref
-              >
-                <a>
-                  <Logo
-                    sx={{
-                      height: 42,
-                      width: 42
-                    }}
-                  />
-                </a>
-              </NextLink>
-            </Box>
-            <Box sx={{ px: 2 }}>
-              <Box
-                onClick={handleOpenOrganizationsPopover}
-                ref={organizationsRef}
-                sx={{
-                  alignItems: 'center',
-                  backgroundColor: 'rgba(255, 255, 255, 0.04)',
-                  cursor: 'pointer',
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  px: 3,
-                  py: '11px',
-                  borderRadius: 1
-                }}
-              >
-                <div>
-                  <Typography
-                    color="inherit"
-                    variant="subtitle1"
-                  >
-                    Acme Inc
-                  </Typography>
-                  <Typography
-                    color="neutral.400"
-                    variant="body2"
-                  >
-                    {t('Your tier')}
-                    {' '}
-                    : Premium
-                  </Typography>
-                </div>
-                <SelectorIcon
-                  sx={{
-                    color: 'neutral.500',
-                    width: 14,
-                    height: 14
-                  }}
-                />
-              </Box>
-            </Box>
-          </div>
-          <Divider
-            sx={{
-              borderColor: '#2D3748', // dark divider
-              my: 3
-            }}
-          />
           <Box sx={{ flexGrow: 1 }}>
             {sections.map((section) => (
               <DashboardSidebarSection
@@ -448,16 +136,10 @@ export const DashboardSidebar: FC<DashboardSidebarProps> = (props) => {
               color="neutral.100"
               variant="subtitle2"
             >
-              {t('Need Help?')}
-            </Typography>
-            <Typography
-              color="neutral.500"
-              variant="body2"
-            >
-              {t('Check our docs')}
+              Learn more about Verifiable Credentials
             </Typography>
             <NextLink
-              href="/docs/welcome"
+              href="https://www.w3.org/TR/vc-data-model/"
               passHref
             >
               <Button
@@ -467,7 +149,7 @@ export const DashboardSidebar: FC<DashboardSidebarProps> = (props) => {
                 sx={{ mt: 2 }}
                 variant="contained"
               >
-                {t('Documentation')}
+                W3C Verifiable Credential
               </Button>
             </NextLink>
           </Box>
